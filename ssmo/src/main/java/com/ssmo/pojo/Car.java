cbd785f9ffa39c8900eced36e7ba4d04b4c0ec62
xuyuan <944034985@qq.com>
2018-01-05 10:02:52 +0800

    add  a car

--- ssmo/src/main/java/com/ssmo/pojo/Car.java
+++ ssmo/src/main/java/com/ssmo/pojo/Car.java
@@ -1,4 +1,16 @@
 package com.ssmo.pojo;
 
 public class Car {
+    private String name;
+
+    public Car() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
